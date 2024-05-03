8d7d3603cac5b98ccad4ad16e08245767a517ebd
joo <wjdwn3132@naver.com>
2024-05-03 23:34:56 +0900

    no message

--- 화면 설계 및 구현/js/q1.js
+++ 화면 설계 및 구현/js/q1.js
@@ -1,42 +1,39 @@
 $(() => {
     var imgNum = 0;
     let timer;
+    var contentCount = $('#contents .content').length;
 
     $('#remote div:eq(0)').addClass('selected');
-    $('#sub .content').hide();
-    $('#sub .content:eq(' + imgNum + ')').show();
-    $('#sub').hide();
-    $('#sub').attr('bottom', 0);
-    $('#sub').show();
-    $('#sub').animate({bottom: 84}, 500);
+    $('#contents .content').eq(imgNum).animate({bottom: 100}, 500);
+    $('#count').html((imgNum + 1) + '/' + contentCount);
 
-    timer = window.setInterval(() => {
-        $('#sub').hide();
-        $('#sub').attr('bottom', 0);
-        
-        if (imgNum > 5)
+    timer = window.setInterval(() => {        
+        if (imgNum > 5) {
             imgNum = 0;
+        }
 
-        $('#count').html((imgNum + 1) + '/6');
-        $('#sub .content').hide();
-        $('#sub .content:eq(' + imgNum + ')').show();
-        $('#sub').show();
-        $('#sub').animate({bottom: 84}, 500);
+        $('#count').html((imgNum + 1) + '/' + contentCount);
+        
+        $('#contents .content').eq(imgNum-1).animate({bottom: 0}, 500);
+        $('#contents .content').eq(imgNum).animate({bottom: 100}, 500, () => {
+            $('#contents .content').eq(imgNum).animate({bottom: 0}, 500);
+        });
+
         $('#imgs').animate({left: -1000 * imgNum}, 500);
         $('#remote div').removeClass('selected');
         $('#remote div:eq(' + (imgNum++) + ')').addClass('selected');
     }, 1500);
 
     $('#remote div').click((e) => {
-        $('#sub').hide();
-        $('#sub').attr('bottom', 0);
         imgNum = $(e.currentTarget).index();
 
-        $('#count').html((imgNum + 1) + '/6');
-        $('#sub .content').hide();
-        $('#sub .content:eq(' + imgNum + ')').show();
-        $('#sub').animate({bottom: 84}, 500);
-        $('#sub').show();
+        $('#count').html((imgNum + 1) + '/' + + contentCount);
+        
+        $('#contents .content').not(':eq(' + imgNum + ')').animate({bottom: 0}, 500);
+        $('#contents .content').eq(imgNum).animate({bottom: 100}, 500, () => {
+            $('#contents .content').eq(imgNum).animate({bottom: 0}, 500);
+        });
+
         $('#imgs').animate({left: -1000 * imgNum}, 500);
         $('#remote div').removeClass('selected');
         $(e.currentTarget).addClass('selected');
